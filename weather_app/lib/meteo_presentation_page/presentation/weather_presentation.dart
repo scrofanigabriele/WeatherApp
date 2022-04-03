4614d0562efec5a3573dbbcef5d49a36275c951a
Gabriele <scrofani.gabriele@gmail.cm>
2022-04-03 21:02:06 +0200

    123

--- weather_app/lib/meteo_presentation_page/presentation/weather_presentation.dart
+++ weather_app/lib/meteo_presentation_page/presentation/weather_presentation.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_typeahead/flutter_typeahead.dart';
 
-import 'weather_row.dart';
-import '../implementation/weather_day.dart';
+import './weather_row.dart';
+import '../../select_city_page/implementation/city_list.dart';
 
 class WeatherPresentation extends StatelessWidget {
   var weatherForecast;
@@ -10,14 +11,55 @@ class WeatherPresentation extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: ListView.builder(
-        padding: const EdgeInsets.all(12),
-        itemCount: weatherForecast.length,
-        itemBuilder: (BuildContext context, int index) {
-          return WeatherRow(weatherForecast[index]);
-        },
-      ),
+    final TextEditingController _typeAheadController = TextEditingController();
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SizedBox(
+            child: TypeAheadFormField(
+              textFieldConfiguration: TextFieldConfiguration(
+                controller: _typeAheadController,
+                decoration: InputDecoration(labelText: 'Select City'),
+              ),
+              suggestionsCallback: (pattern) async {
+                return cities;
+              },
+              itemBuilder: (context, suggestion) {
+                return Container(
+                  child: ListTile(
+                    title: Text(suggestion.toString()),
+                  ),
+                );
+              },
+              onSaved: (suggestion) {
+                print('saved');
+                // selectedCity.changeSelection(suggestion.toString());
+              },
+              validator: (suggestion) {
+                var exist =
+                    cities.where((element) => element == suggestion.toString());
+                if (exist == null) {
+                  print('errore');
+                  return 'select a proper city form the list';
+                }
+              },
+              onSuggestionSelected: (suggestion) {
+                // selectedCity.changeSelection(suggestion.toString());
+                // Navigator.of(context).popAndPushNamed(SearchPage.routeName);
+              },
+            ),
+            height: 40,
+          ),
+        ),
+        ListView.builder(
+          padding: const EdgeInsets.all(12),
+          itemCount: weatherForecast.length,
+          itemBuilder: (BuildContext context, int index) {
+            return WeatherRow(weatherForecast[index]);
+          },
+        ),
+      ],
     );
   }
 }
